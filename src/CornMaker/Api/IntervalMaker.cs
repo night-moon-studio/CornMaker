ed07815c885f34470b8f424362195fb58fbbf091
NMSAzulX <16236903+NMSAzulX@users.noreply.github.com>
2020-11-18 22:37:22 +0800

    增加周期 CORN 表达式 #1

---
+++ src/CornMaker/Api/IntervalMaker.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+
+public class IntervalMaker
+{
+
+    private long _current;
+    private long _timespan;
+    private readonly CornMaker _maker;
+    private IntervalMaker(long? timespan = null, long? startTime = null)
+    {
+
+        _maker = CornMaker.Create();
+        _timespan = timespan.HasValue ? timespan.Value : 0;
+        _current = startTime.HasValue ? startTime.Value : DateTime.Now.TimestampSeconds();
+
+    }
+
+    /// <summary>
+    /// 时间间隔天数
+    /// </summary>
+    /// <param name="days">天数</param>
+    /// <returns></returns>
+    public IntervalMaker Days(long days)
+    {
+        _timespan += days * 86400;
+        return this;
+    }
+
+
+
+
+    /// <summary>
+    /// 时间间隔小时数
+    /// </summary>
+    /// <param name="hours">小时数</param>
+    /// <returns></returns>
+    public IntervalMaker Hours(long hours)
+    {
+        _timespan += hours * 3600;
+        return this;
+    }
+
+
+
+
+    /// <summary>
+    /// 时间间隔分钟数
+    /// </summary>
+    /// <param name="minutes">分钟数</param>
+    /// <returns></returns>
+    public IntervalMaker Minutes(long minutes)
+    {
+        _timespan += minutes * 60;
+        return this;
+    }
+
+
+
+    /// <summary>
+    /// 时间间隔秒数
+    /// </summary>
+    /// <param name="seconds">秒数</param>
+    /// <returns></returns>
+    public IntervalMaker Seconds(long seconds)
+    {
+        _timespan += seconds;
+        return this;
+    }
+
+
+
+
+
+    /// <summary>
+    /// 获取下一个定时表达式
+    /// </summary>
+    /// <returns></returns>
+    public string NextCorn()
+    {
+        _current += _timespan;
+        _maker.Clear();
+        return _maker.GetFromDateTime(_current.DateTimeFromSeconds()).GetCornScript();
+    }
+
+
+    public static IntervalMaker Create(long? timespan = null, long? startTime = null)
+    {
+        return new IntervalMaker(timespan);
+    }
+
+}
+
---
+++ src/CornMaker/Extension/DateTimeExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+internal static class DateTimeExtension
+{
+    private static readonly DateTime _startTime;
+    static DateTimeExtension()
+    {
+        _startTime = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+    }
+    public static long TimestampSeconds(this DateTime dateTime)
+    {
+        return (long)(dateTime.ToUniversalTime() - _startTime).TotalSeconds;
+    }
+    public static DateTime DateTimeFromSeconds(this long timestamp)
+    {
+        return _startTime.AddSeconds(timestamp).ToLocalTime();
+    }
+    
+}
+
